0685b097572ce088a4c528550f9973d9b52a4ea1
XxViRUsPRO <xxvirus.15@hotmail.com>
2024-10-11 03:06:41 +0300

    feat: added giscus for blogs

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+import type { GiscusProps } from "@giscus/react";
+
+export const GISCUS_CONFIG: GiscusProps = {
+  repo: "xxviruspro/xxvirus.pro",
+  repoId: "R_kgDOM8DYAA",
+  mapping: "url",
+  category: "Comments",
+  categoryId: "DIC_kwDOM8DYAM4CjPbo",
+  strict: "0",
+  reactionsEnabled: "1",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  theme: "preferred_color_scheme",
+  lang: "en",
+  loading: "lazy",
+};
